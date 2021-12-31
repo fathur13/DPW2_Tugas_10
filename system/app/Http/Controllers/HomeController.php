31dca06ce45eab22ada4cdfcfb4a107e201bd800
fikrizulfikarb <fathur34rahman56@gmail.com>
2021-12-31 16:37:24 +0700

    Initial commit

---
+++ system/app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller{
+
+	function showberanda(){
+		return view('template.beranda');
+	}
+
+	function showproduk(){
+		return view('template.produk');
+	}
+
+
+	function client(){
+		return view('client.client');
+	}
+
+		function test($produk){
+			echo "Nama produk $produk";
+	
+	}
+
+	
+
+
+
+}
